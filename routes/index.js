9db6dc8a542dbd0e75c1eca9882a36babc64d185
Axelle <crea@flipnpik.eu>
2021-05-18 13:47:24 +0200

    sign up

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,69 @@
 var express = require('express');
 var router = express.Router();
 
+var uid2 = require('uid2')
+var bcrypt = require('bcrypt');
+
+var userModel = require('../models/users')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+// route inscription
+router.post('/sign-up', async function(req,res,next){
+
+  var error = []
+  var result = false
+  var saveUser = null
+  var token = null
+
+
+  const data = await userModel.findOne({
+    email: req.body.Email
+  })
+
+  if(data != null){
+    error.push('utilisateur déjà présent')
+  }
+
+  if(
+     req.body.Email == ''
+  || req.body.Password == ''
+  ){
+    error.push('Les champs sont vides')
+  }
+
+
+  if(error.length == 0){
+
+    var hash = bcrypt.hashSync(req.body.Password, 10);
+    var newUser = new userModel({
+      Firstname: req.body.Firstname,
+      Email: req.body.Email,
+      Password: hash,
+      token: uid2(32),
+      Age: req.body.Age,
+      Description: req.body.Description,
+      FavoritesSports: req.body.FavoritesSports,
+      SportsHabits: req.body.SportsHabits,
+      SportsHours: req.body.SportsHours,
+      UserPicture: req.body.UserPicture,
+    })
+  
+    saveUser = await newUser.save()
+  
+    
+    if(saveUser){
+      result = true
+      token = saveUser.token
+    }
+  }
+  
+
+  res.json({result, saveUser, error, token})
+})
 
 module.exports = router;
