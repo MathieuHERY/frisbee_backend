acc73a9bcfbf079f8e265364f0e7bf2ffa90ae27
Marilène <67200994+MarileneK@users.noreply.github.com>
2021-05-18 14:16:07 +0200

    get work from Axelle

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var usersModel = require("../models/usersSchema"); // LIEN+NOM DE LA CONSTANTE À AJUSTER // Import du modèle Users
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
 
 module.exports = router;
