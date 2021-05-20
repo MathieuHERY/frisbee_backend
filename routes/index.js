4a45c321e8e0f8fbc3a8bfb8b0a1694c2ddcf7f2
Marilène <67200994+MarileneK@users.noreply.github.com>
2021-05-20 10:08:23 +0200

    get latest version

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var uid2 = require('uid2')
 var bcrypt = require('bcrypt');
 
 var userModel = require('../models/users'); // Import du modèle Users
+const { updateOne } = require('../models/users');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -62,20 +63,37 @@ router.post('/sign-up', async function(req,res,next){
     }
   }
 
-
-  
-
   res.json({result, saveUser, error, token})
 })
 
-// USERSSCREEN ROUTE - SHOW ALL USERS
+// USER ROUTE - SHOW ALL USERS
+// Je récupère TOUS les utilisateurs dans la BDD que je vais filtrer en front
 router.get('/users', async function(req, res, next) {
   
-  var usersData = await userModel.find() ; // Je récupère TOUS les utilisateurs dans la BDD
+  var usersData = await userModel.find() ; 
 
   res.json({usersData})
 
 })
+
+
+// MY LOCATION ROUTE - GET MY OWN LATITUDE AND LONGITUDE
+// if myLocation est à true : faire un update sur la collection User avec mon ID (dans MongoDB) et mettre à jour la latitude et longitude
+// else ne fait rien
+router.post('/my-location', async function(req, res, next) {
+  var myLocation = await userModel.findById(token);
+
+  if (myLocation) {
+    const currentLocation = userModel.updateOne({UserLatitude: '', UserLongitude: ''}) // besoin de dynamiser la latitude et la longitude
+    
+    res.json({ result: true, currentLocation: currentLocation })
+
+  } else {
+    res.json({ result: false })
+  }
+
+})
+
 
 
 module.exports = router;
