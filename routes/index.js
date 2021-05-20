c7be09a682fc8dbb7f5b0b67ad036ce30fe34d96
Axelle <crea@flipnpik.eu>
2021-05-20 09:52:09 +0200

    essai route sign-in

--- routes/index.js
+++ routes/index.js
@@ -29,14 +29,6 @@ router.post('/sign-up', async function(req,res,next){
     error.push('utilisateur déjà présent')
   }
 
-  if(
-     req.body.Email == ''
-  || req.body.Password == ''
-  ){
-    error.push('Les champs sont vides')
-  }
-
-
   if(error.length == 0){
 
     var hash = bcrypt.hashSync(req.body.Password, 10);
@@ -64,6 +56,47 @@ router.post('/sign-up', async function(req,res,next){
   
 
   res.json({result, saveUser, error, token})
+})
+
+//route connexion 
+
+router.post('/sign-in', async function(req,res,next){
+
+  var result = false
+  var user = null
+  var error = []
+  var token = null
+  
+  if(req.body.Email == ''
+  || req.body.Password == ''
+  ){
+    error.push('Les champs sont vides')
+  }
+
+  if(error.length == 0){
+    const user = await userModel.findOne({
+      Email: req.body.Email,
+    })
+  
+    
+    if(user){
+      if(bcrypt.compareSync(req.body.Password, user.Password)){
+        result = true
+        token = user.token
+      } else {
+        result = false
+        error.push('mot de passe incorrect')
+      }
+      
+    } else {
+      error.push('email incorrect')
+    }
+  }
+  
+
+  res.json({result, user, error, token})
+
+
 })
 
 module.exports = router;
