9609f8535e767896daa08d85f9bafb40fbe72a7e
Axelle <crea@flipnpik.eu>
2021-05-25 10:10:51 +0200

    monprofil

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ cloudinary.config({
  });
 
 
-var userModel = require('../models/users'); // Import du modèle Users
+var userModel = require('../models/users'); 
 const { updateOne } = require('../models/users');
 var placesModel = require('../models/places')
 
@@ -70,8 +70,8 @@ router.post('/sign-up', async function(req,res,next){
   res.json({result, saveUser, error, token})
 })
 
-/* POST Upload picture received from app. */
 
+/* POST Upload picture received from app. */
 router.post('/upload-user-picture', async (req, res, next) => {
 
   console.log(req.files.picture)
@@ -95,8 +95,7 @@ router.post('/upload-user-picture', async (req, res, next) => {
 )
 
 
-//route connexion 
-
+//connexion 
 router.post('/sign-in', async function(req,res,next){
 
   var result = false;
@@ -114,8 +113,6 @@ router.post('/sign-in', async function(req,res,next){
     const user = await userModel.findOne({
       Email: req.body.Email,
     })
-  
-    
     if(user){
       if(bcrypt.compareSync(req.body.Password, user.Password)){
         result = true
@@ -124,7 +121,6 @@ router.post('/sign-in', async function(req,res,next){
         result = false
         error.push('mot de passe incorrect')
       }
-      
     } else {
       error.push('email incorrect')
     }
@@ -136,28 +132,20 @@ router.post('/sign-in', async function(req,res,next){
 
 
 // USER ROUTE - SHOW ALL USERS
-// Je récupère TOUS les utilisateurs dans la BDD que je vais filtrer en front
-router.get('/users-filtered', async function(req, res, next) {
-  
+router.get('/users-filtered', async function(req, res, next) { 
   var usersData = await userModel.find() ; 
-  // console.log(usersData, 'je récupère tous les users côté back')
-
   res.json({usersData})
-
 })
 
 // get Pins to display to Map
 router.get('/places', async function(req, res, next) {
-
   var PinsData = await placesModel.find()
-
   res.json({PinsData})
-
 })
 
 
 
-//new Pins
+//add new Pins
 router.post("/newplace", async function (req, res, next) {
   var newPlace = new placesModel({
     name: req.body.name,
@@ -172,5 +160,19 @@ router.post("/newplace", async function (req, res, next) {
   savePlace = await newPlace.save()
   res.json({result:true})
 })
+
+
+//infos mon profil login 
+router.get('/user', async function(req, res, next) { 
+  var userData = await userModel.find() ; 
+  res.json({userData})
+})
+
+
+/* // Logout
+router.get('/logout', async (req, res, next) => {
+  req.session.user = null;
+});
+ */
 
 module.exports = router;
